36b046a7eb31c04f6b4415ac6cfe03c9cf1a62e6
Snowball <riteshraushan30@gmail.com>
2023-10-25 14:24:30 +0530

    Bug Fixed ✅

--- config.py
+++ config.py
@@ -6,21 +6,21 @@ id_pattern = re.compile(r'^.\d+$')
 class Config(object):
 
     # Client Config 
-    API_ID = int(os.environ.get('API_ID', '21288218')) # ⚠️ Required
-    API_HASH = os.environ.get('API_HASH', 'dd47d5c4fbc31534aa764ef9918b3acd') # ⚠️ Required
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '6473742180:AAGVFyaSXpwxtuOl4EY_lU1PBW1YUQ6GwGE') # ⚠️ Required
+    API_ID = int(os.environ.get('API_ID', '')) # ⚠️ Required
+    API_HASH = os.environ.get('API_HASH', '') # ⚠️ Required
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '') # ⚠️ Required
 
     # database config
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://TEST:TEST@cluster0.ehigmtx.mongodb.net/?retryWrites=true&w=majority") # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL","") # ⚠️ Required
 
     # Other Config 
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "FORWARDRUSSIABOT") # ⚠️ Required
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # ⚠️ Required
     BOT_UPTIME  = time.time()
-    OWNER = int(os.environ.get('OWNER', '5690679398')) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001971176803")) # ⚠️ Required
+    OWNER = int(os.environ.get('OWNER', '')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # ⚠️ Required
     APPROVED_WELCOME_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYou're Auto Approved. ✅")
     LEAVING_BY_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "👋 Bye {mention} !\nSee You Soon by {title}\n\nYou Left. ⛔")
-    FORCE_SUB = os.environ.get('FORCE_SUB', '-1001956303989') # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None 
 
